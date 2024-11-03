843562077ec5c90bec2b6c151fb2730ce64c246a
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-11-03 12:12:32 -0600

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -26,5 +26,10 @@ def pag4():
 def pag5():
     return render_template('nivelPri.html')
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
